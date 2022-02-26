55f22ec9df7027eb498bd6415c8b4b6766474eb7
André Wählisch <andrewaehlisch@users.noreply.github.com>
2022-02-26 10:45:11 +0100

    apply frame strata changes required for retail Shadowlands release of patch 9.2

--- ClassIcons.lua
+++ ClassIcons.lua
@@ -304,10 +304,6 @@ function ClassIcons_OnEvent(self, event, arg1)
 	end;
 end;
 
-function ClassIcons_Icon_OnLoad(self)
-	self:SetFrameLevel(self:GetFrameLevel()+2);
-end;
-
 function ClassIcons_UpdatePlayerFrame()
 	ClassIcons_UpdateIcon("PlayerFrame", "player", CLASSICONS_CONFIG.Player);
 end;
